bab5298726052c1a3300bdea7759c366eeca4699
booger-man2020 <vatsevicho@gmail.com>
2021-11-15 21:34:15 +0300

    нажал ctrl+shift+l

--- src/main/java/HomeWorkApp.java
+++ src/main/java/HomeWorkApp.java
@@ -6,14 +6,15 @@ public class HomeWorkApp {
         chekSumSing();
         printColor();
         compareNumbers();
-
     }
+
     public static void printTheeWords() {
         System.out.println("Задача 1: Слова");
         System.out.println("Orange");
         System.out.println("Banana");
         System.out.println("Apple");
     }
+
     public static void chekSumSing() {
         System.out.println("Задача 2: Сумма чисел");
         Scanner scanner = new Scanner(System.in);
@@ -25,6 +26,7 @@ public class HomeWorkApp {
             System.out.println("Сумма положительная");
         } else System.out.println("Сумма отрицательная");
         }
+
         public static void printColor() {
             System.out.println("Задача 3:  Цвет числа");
             Scanner scanner = new Scanner(System.in);
@@ -36,19 +38,16 @@ public class HomeWorkApp {
             System.out.println("Зеленый");
         } else System.out.println("Желтый");
         }
+
         public static void compareNumbers(){
             System.out.println("Задача 4: Сравнение чисел");
             Scanner scanner = new Scanner(System.in);
             System.out.println("Введи чиcло а");
-        int a = scanner.nextInt();
+            int a = scanner.nextInt();
             System.out.println("Введи число b");
             int b = scanner.nextInt();
             if ( a >= b){
                 System.out.println(" а >= b");
             } else System.out.println("a < b");
-
         }
-
-
-
     }
