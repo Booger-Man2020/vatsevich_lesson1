0a36a8bf39c963eac47d1ddb5c7477e317237949
booger-man2020 <vatsevicho@gmail.com>
2021-11-16 20:59:43 +0300

    нажал ctrl+alt+l

--- src/main/java/HomeWorkApp.java
+++ src/main/java/HomeWorkApp.java
@@ -25,29 +25,29 @@ public class HomeWorkApp {
         if (a + b > 0) {
             System.out.println("Сумма положительная");
         } else System.out.println("Сумма отрицательная");
-        }
+    }
 
-        public static void printColor() {
-            System.out.println("Задача 3:  Цвет числа");
-            Scanner scanner = new Scanner(System.in);
-            System.out.println("Введите число");
+    public static void printColor() {
+        System.out.println("Задача 3:  Цвет числа");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите число");
         int value = scanner.nextInt();
-        if (value <= 0 ) {
+        if (value <= 0) {
             System.out.println("Красный");
-        } else if (value > 100){
+        } else if (value > 100) {
             System.out.println("Зеленый");
         } else System.out.println("Желтый");
-        }
-
-        public static void compareNumbers(){
-            System.out.println("Задача 4: Сравнение чисел");
-            Scanner scanner = new Scanner(System.in);
-            System.out.println("Введи чиcло а");
-            int a = scanner.nextInt();
-            System.out.println("Введи число b");
-            int b = scanner.nextInt();
-            if ( a >= b){
-                System.out.println(" а >= b");
-            } else System.out.println("a < b");
-        }
     }
+
+    public static void compareNumbers() {
+        System.out.println("Задача 4: Сравнение чисел");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введи чиcло а");
+        int a = scanner.nextInt();
+        System.out.println("Введи число b");
+        int b = scanner.nextInt();
+        if (a >= b) {
+            System.out.println(" а >= b");
+        } else System.out.println("a < b");
+    }
+}
